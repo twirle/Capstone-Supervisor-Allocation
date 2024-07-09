3deffc446c2d3257ded3da5d4ef69b0133a48448
twirle <acrylicblaze@gmail.com>
2024-07-09 22:54:12 +0800

    include vercel.json

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://server-kohl-eight.vercel.app"], // Change this to match your frontend URL
+    origin: ["http://client-kohl-eight.vercel.app"], // Change this to match your frontend URL
     credentials: true,
   })
 );
